f9ff3672fa0a1d8024266135be6cec34e7b6dcca
CybronVaughan <cybronvaughan@users.noreply.github.com>
2018-03-31 15:58:42 -0300

    Commit 01
    Importação dos arquivos do protótio da UC05, já com a programação do pulo finalizada.

---
+++ Alien City/Assets/Scripts/ScriptPedra.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptPedra {
+
+public class ScriptPedra : MonoBehaviour {
+
+		void OnTriggerEnter2D(Collider2D col)
+		{
+			if (col.CompareTag ("Player")) {
+				Destroy (this.gameObject);
+				col.gameObject.GetComponent<PlayerController> ().pontuacaoPlayer++;
+			}
+		}
+}}
