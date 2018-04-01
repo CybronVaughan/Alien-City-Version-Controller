39bb76c269a4c01379010445c43a05c4cac0364d
CybronVaughan <cybronvaughan@users.noreply.github.com>
2018-04-01 19:41:22 -0300

    Commit 03
    Foi implementada a programação do tiro! Falta programar a Mensagem de Controle, a finalização (vitória) da fase e sons.

--- Alien City/Assets/Scripts/BulletScript.cs
+++ Alien City/Assets/Scripts/BulletScript.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
 
+	public float BulletTimer = 3f;
+
+	private Vector2 speed = new Vector2 (55, 0);
+	private Rigidbody2D rbBullet;
+
 	// Use this for initialization
 	void Start () {
+		rbBullet = GetComponent<Rigidbody2D>();
+		rbBullet.velocity = speed * this.transform.localScale.x;
+		Destroy (gameObject, BulletTimer);
 		
 	}
 	
