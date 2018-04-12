8bcc659c139dbd12808167a0eac62e4ee8468b38
CybronVaughan <cybronvaughan@users.noreply.github.com>
2018-04-12 16:18:35 -0300

    Commit 08
    Implementação de nova arma e HUD.

---
+++ Alien City/Assets/Scripts/GrenadeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeScript : MonoBehaviour {
+
+	public float GrenadeTimer = 3f;
+
+	private Vector2 speed = new Vector2 (75, 0);
+	private Rigidbody2D rbGrenade;
+
+	// Use this for initialization
+	void Start () {
+		rbGrenade = GetComponent<Rigidbody2D>();
+		rbGrenade.velocity = speed * this.transform.localScale.x;
+		Destroy (gameObject, GrenadeTimer);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
